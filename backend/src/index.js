de7602335fc63034b1a825e964399844f42d1805
sazisblu <sahikarmisazan@gmail.com>
2026-02-22 17:21:00 +0545

    feat():user authentication , created singup,login,logout endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import authroutes from "./routes/user-auth.route.js";
+import "dotenv/config";
 
 const app = express();
+app.use(express.json());
+
+app.use("/auth", authroutes);
 
 app.get("/test", (req, res) => {
   res.status(200).json({
